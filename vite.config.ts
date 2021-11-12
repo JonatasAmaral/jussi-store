182c7a3c2cc7a1e19d36f480243af6e0290f3a83
Jonatas Amaral <jonataspadtab@gmail.com>
2021-11-11 21:48:33 -0300

    install SVG files support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import reactSvgPlugin from "vite-plugin-react-svg";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
 			"~": resolve(__dirname, "src"),
 		},
 	},
-	plugins: [react()],
+	plugins: [react(), reactSvgPlugin({ defaultExport: "component" })],
 });
