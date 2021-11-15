07a37b4e6f8223594e8665ef39c26f12395d68c5
Jonatas Amaral <jonataspadtab@gmail.com>
2021-11-15 15:46:20 -0300

    add resposivity to newsletter section
    # Conflicts:
    #	src/components/Newsletter/styles.scss

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -10,9 +10,10 @@ export function Newsletter(props: PropsType) {
 	return (
 		<section className="newsletter">
 			<h3>receba novidades da nossa área de produtos digitais.</h3>
-			<form action="#">
+			<form action="">
 				<div className="wrapper email">
 					<input type="email" required placeholder="Digite seu e-mail" />
+					<input type="submit" hidden />
 				</div>
 				<div className="wrapper send">
 					<button type="submit">cadastrar</button>
