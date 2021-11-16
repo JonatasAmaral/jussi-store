6801bb5c0111689b6cfd02cfca87687e7878a32c
Jonatas Amaral <jonataspadtab@gmail.com>
2021-11-16 10:42:44 -0300

    first iteration off mobile hamburger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,6 +13,7 @@ export function Header() {
 	const [validQuery, setValidQuery] = React.useState(false);
 	const [apiResponseData, setApiResponseData] = React.useState<any>({});
 	const [hasData, setHasData] = React.useState(false);
+	const [mobMenuOpen, setMobMenuClosed] = React.useState<boolean>(true);
 
 	function handleLogin(e: MouseEvent) {
 		e.preventDefault();
@@ -48,63 +49,85 @@ export function Header() {
 		<>
 			<header className={`${hasData && "blur"}`} tabIndex={-1}>
 				<Logo />
-				<nav>
-					<ul>
-						<li>
-							<a href="/#solutions">Nossas soluções</a>
-						</li>
-						<li>
-							<a href="/#sobre-nos">Conheça a Jüssi</a>
-						</li>
-					</ul>
-				</nav>
-
-				<form action="" className="search" onSubmit={handleSearch}>
-					<input
-						id="search-input"
-						name="search-input"
-						type="text"
-						placeholder="Buscar um pokémon"
-						required
-						value={searchQuery}
-						onChange={(e) => {
-							setValidQuery(e.target.validity.valid);
-							setSearchQuery(e.target.value);
-						}}
-						pattern="([0-9]{1,})|(^[a-zA-Z]{3,15}$)"
-						onInvalid={(e) => {
-							alert("Digite um número, ou um nome com 3 à 15 letras");
-						}}
-					/>
-					<input type="submit" hidden />
-					<label htmlFor="search-input">
-						{/* HACK: make label icon look EXACLY the sabe as the one from submit button*/}
-						<button
-							className="button"
-							disabled
-							style={{ pointerEvents: "none" }}
-						>
-							<FiSearch />
-						</button>
-					</label>
-					<button className="button" type="submit" disabled={!validQuery}>
-						<FiSearch />
+				<label
+					htmlFor="isMenuOpen"
+					className="mob-menu open"
+					onClick={(e) => {
+						e.target;
+						setMobMenuClosed((p) => !p);
+					}}
+				>
+					<button className="button cart">
+						{mobMenuOpen ? <FiMenu /> : <FiX />}
 					</button>
-				</form>
+				</label>
+				<input
+					type="checkbox"
+					name="isMenuOpen"
+					id="isMenuOpen"
+					checked={mobMenuOpen}
+					hidden
+				/>
+				<div className="header-menu">
+					<nav>
+						<ul>
+							<li>
+								<a href="/#solutions">Nossas soluções</a>
+							</li>
+							<li>
+								<a href="/#sobre-nos">Conheça a Jüssi</a>
+							</li>
+						</ul>
+					</nav>
 
-				<a className="login" href="#" onClick={handleLogin}>
-					{logged ? (
-						<>
-							<FiUser size={20} style={{ paddingRight: 5 }} />
-							Logout
-						</>
-					) : (
-						"Login"
-					)}
-				</a>
-				<button className="button cart">
-					<FiShoppingCart />
-				</button>
+					<div className="wrapper">
+						<form action="" className="search" onSubmit={handleSearch}>
+							<input
+								id="search-input"
+								name="search-input"
+								type="text"
+								placeholder="Buscar um pokémon"
+								required
+								value={searchQuery}
+								onChange={(e) => {
+									setValidQuery(e.target.validity.valid);
+									setSearchQuery(e.target.value);
+								}}
+								pattern="([0-9]{1,})|(^[a-zA-Z]{3,15}$)"
+								onInvalid={(e) => {
+									alert("Digite um número, ou um nome com 3 à 15 letras");
+								}}
+							/>
+							<input type="submit" hidden />
+							<label htmlFor="search-input">
+								{/* HACK: make label icon look EXACLY the sabe as the one from submit button*/}
+								<button
+									className="button"
+									disabled
+									style={{ pointerEvents: "none" }}
+								>
+									<FiSearch />
+								</button>
+							</label>
+							<button className="button" type="submit" disabled={!validQuery}>
+								<FiSearch />
+							</button>
+						</form>
+						<a className="login" href="#" onClick={handleLogin}>
+							{logged ? (
+								<>
+									<FiUser size={20} style={{ paddingRight: 5 }} />
+									Logout
+								</>
+							) : (
+								"Login"
+							)}
+						</a>
+						<button className="button cart">
+							<FiShoppingCart />
+						</button>
+					</div>
+				</div>
 			</header>
 
 			{hasData && (
